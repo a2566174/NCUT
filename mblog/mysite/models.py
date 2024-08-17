1ce0e62611ea35a376d3607fc064b3aa5f0ee382
a2566174 <ham631228@gmail.com>
2024-08-18 00:00:53 +0800

    test2

--- mblog/mysite/models.py
+++ mblog/mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug  = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=False)
+
+    class Meta:
+        ordering = ('-pub_date')
+    def _str_(self):
+        return self.title     
+
 # Create your models here.
