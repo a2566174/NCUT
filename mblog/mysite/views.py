fab0f11c024db109dd46a56bce151058a415e599
a2566174 <ham631228@gmail.com>
2024-08-19 02:28:11 +0800

    i am tired

--- mblog/mysite/views.py
+++ mblog/mysite/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import post
+from datetime import datetime
+
+def homepage(resquest):
+    posts = post.objects.all()
+    now = datetime.now()
+    return render(request,"index.html",locals())
+    post_lists = list()
+    for count,post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count))+str(post)+"<br>")
+    return HttpResponse(post_lists)
+
 
 # Create your views here.
