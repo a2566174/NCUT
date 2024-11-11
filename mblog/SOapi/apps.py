f425565a4132154994e66705eec680d3e6032af0
a2566174 <ham631228@gmail.com>
2024-11-12 01:09:12 +0800

    a

---
+++ mblog/SOapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SoapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SOapi'
--- mblog/apij/views.py
+++ mblog/apij/views.py
@@ -5,12 +5,12 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def load_json_view(request):
-    # 获取JSON文件路径
+    # JSON文件路徑
     json_file_path = os.path.join(os.path.dirname(__file__), 'static/package.json')
 
-    # 读取JSON文件
+    # 讀取JSON文件
     with open(json_file_path, 'r') as file:
         data = json.load(file)
 
-    # 返回JSON响应
+    # 返回JSON
     return Response(data)
