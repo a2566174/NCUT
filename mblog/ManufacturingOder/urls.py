c293a1ab17d085042154fe6d069d4ece6af9cc91
a2566174 <ham631228@gmail.com>
2024-11-20 02:37:44 +0800

    a

--- mblog/ManufacturingOder/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.display_data, name='display_data'),
-    path('remove_item/<str:order_id>/', views.remove_item, name='remove_item'),
-    path('refresh/', views.refresh_data, name='refresh_data'),
-]
